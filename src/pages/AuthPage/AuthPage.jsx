730cfe4d97560aeffba45777c109d8e6af30923a
Timerk <149791526+Timerk@users.noreply.github.com>
2024-10-10 21:22:37 +0200

    Auth Page form added
    -added form for login in and signing up

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -5,18 +5,19 @@ export const AuthPage = () => {
   return (
     <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
         <Container maxW={"container.md"} padding={0}>
-            <Box display={{base: "none", md:"block"}}>
-                <Image src='/auth.png' h={650} alt='Phone img' />
-            </Box>
-
-            <VStack spacing={4} align={"stretch"}>
-                <AuthForm/>
-                <Box textAlign={"center"}>Get the app.</Box>
-                <Flex gap={5} justifyContent={"center"}>
-                    <Image src='/google.png' h={10} alt="playstore img"/>
-                    <Image src="/microsoft.png" h={10} alt="microsoft img"/>
-                </Flex>
-            </VStack>
+            <Flex justifyContent={ "center" } alignItems={ "center" } gap={10}>
+                <Box display={{base: "none", md:"block"}}>
+                    <Image src='/auth.png' h={650} alt='Phone img' />
+                </Box>
+                <VStack spacing={4} align={"stretch"}>
+                    <AuthForm/>
+                    <Box textAlign={"center"}>Get the app.</Box>
+                    <Flex gap={5} justifyContent={"center"}>
+                        <Image src='/google.png' h={10} alt="playstore img"/>
+                        <Image src="/microsoft.png" h={10} alt="microsoft img"/>
+                    </Flex>
+                </VStack>
+            </Flex>
         </Container>
     </Flex>
   )
