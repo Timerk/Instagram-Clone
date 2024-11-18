63e63ce4ff61be104cf518a6a4d074cd8df67339
Tim Berk <timberk3@googlemail.com>
2024-11-18 16:03:17 +0100

    -added Hook to fetch Profile data based on user name -updated ProfileHeader to display fetched data

--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -15,7 +15,7 @@ export const ProfilePosts = ({avatar}) => {
   return (
     <Grid templateColumns={{sm:"repeat(1, 1fr)", md:"repeat(3, 1fr)"}} gap={1} columnGap={1}>
       {isLoading && [0, 1, 2, 3, 4, 5].map((_, idx) =>(
-        <Skeleton w={"full"}>
+        <Skeleton key={idx} w={"full"}>
           <Box h={"300px"}>contents</Box>
         </Skeleton>
       ))}
--- src/hooks/useGetUserProfileByUsername.js
+++ src/hooks/useGetUserProfileByUsername.js
@@ -1,22 +1,39 @@
-import { useState, useEffect } from "react";
-import useUserProfileStore from "../store/userProfileStore";
+import { useEffect, useState } from "react";
 import useShowToast from "./useShowToast";
-import { collection, getDoc, query, where } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { firestore } from "../firebase/firebase";
+import useUserProfileStore from "../store/userProfileStore";
 
 const useGetUserProfileByUsername = (username) => {
-    const[isLoading, setIsLoading] = useState(false)
-    const showToast = useShowToast();
+	const [isLoading, setIsLoading] = useState(true);
+	const showToast = useShowToast();
+	const { userProfile, setUserProfile } = useUserProfileStore();
 
-    useEffect(() => {
-        const getUserProfile = async () =>{
-            setIsLoading(true)
-            try {
-                const q = query(collection(firestore,"users",where("username", "==", username)))
-                const querySnapShot = await getDoc(q)
-            } catch (error) {
-                showToast("Error", error.message, "error")
-            }
-        }
-    },[])
-}
+	useEffect(() => {
+		const getUserProfile = async () => {
+			setIsLoading(true);
+			try {
+				const q = query(collection(firestore, "users"), where("username", "==", username.username));
+				const querySnapshot = await getDocs(q);
+
+				if (querySnapshot.empty) return setUserProfile(null);
+
+				let userDoc;
+				querySnapshot.forEach((doc) => {
+					userDoc = doc.data();
+				});
+
+				setUserProfile(userDoc);
+			} catch (error) {
+				showToast("Error", error.message, "error");
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		getUserProfile();
+	}, [setUserProfile, username, showToast]);
+
+	return { isLoading, userProfile };
+};
+
+export default useGetUserProfileByUsername;
