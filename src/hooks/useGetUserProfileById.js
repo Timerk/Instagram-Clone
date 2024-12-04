8926eb87542ea051b12cb112a6145a7932fe0b9b
Timerk <149791526+Timerk@users.noreply.github.com>
2024-12-04 20:39:57 +0100

    -added hook to getUserProfile by id -added information from useGetUserProfileById hook to Comment

---
+++ src/hooks/useGetUserProfileById.js
@@ -0,0 +1,34 @@
+import useShowToast from "./useShowToast";
+import { useState, useEffect } from "react"
+import { doc, getDoc } from "firebase/firestore"
+import { firestore } from "../firebase/firebase"
+const useGetUserProfileById = (userId) => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [userProfile, setUserProfile] = useState(null)
+    const showToast = useShowToast()
+
+    useEffect(() => {
+        const getUserProfile = async () => {
+            setIsLoading(true);
+            setUserProfile(null);
+            try {
+                const userRef = await getDoc(doc(firestore, "users", userId));
+
+                if (userRef.exists()) {
+                    setUserProfile(userRef.data());
+                }
+            }
+            catch (error) {
+                showToast("Error", error.message, "error");
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        getUserProfile();
+    },[setUserProfile, userId, showToast])
+
+    return { isLoading, userProfile }
+}
+
+export default useGetUserProfileById
+
