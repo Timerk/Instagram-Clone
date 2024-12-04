f1bb032fdb326fb7691118b67fca99fe1725d318
Timerk <149791526+Timerk@users.noreply.github.com>
2024-12-04 19:51:39 +0100

    -added hook for comment creation -replaced placeholder with real comments

--- src/components/FeedPosts/PostFooder.jsx
+++ src/components/FeedPosts/PostFooder.jsx
@@ -1,11 +1,29 @@
 import { Flex, Box, Text, InputGroup, Input, InputRightElement, Button } from "@chakra-ui/react"
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { NotificationsLogo, UnlikeLogo, CommentLogo } from "../../assets/constants"
+import useCreateComment from "../../hooks/useCreateComment"
+import useShowToast from "../../hooks/useShowToast"
+import useAuthStore from "../../store/authStore"
 
-export const PostFooder = ({username, isProfilePost}) => {
+export const PostFooder = ({post, isProfilePost}) => {
 
   const [liked, setLiked] = useState(false)
   const [likes, setLikes] = useState(1000)
+  const {isUploading, handlePostComment} = useCreateComment()
+  const [comment , setComment] = useState("")
+  const showToast = useShowToast()
+  const commentRef = useRef(null)
+  const authUser = useAuthStore((state) => state.user)
+
+  const handleSubmitComment = async() =>{
+    try {
+      await handlePostComment(post.id, comment)
+      setComment("")
+    } catch (error) {
+      showToast("Error", error.message, "error")
+    }
+
+  }
 
   const handleLike = () => {
     if (liked) {
@@ -19,35 +37,44 @@ export const PostFooder = ({username, isProfilePost}) => {
 
   return (
     <Box mb={10} marginTop={"auto"}>
-      <Flex alignItems={"center"} gap={4} mt={4} w={"full"} pt={0} mb={2}>
-        <Box cursor={"pointer"} onClick={handleLike}>
-          {!liked ? <NotificationsLogo/> : <UnlikeLogo/>}
-        </Box>
-        <Box cursor={"pointer"} fontSize={18}>
-          <CommentLogo/>
-        </Box>
-      </Flex>
+      {authUser && (
+        <Flex alignItems={"center"} gap={4} mt={4} w={"full"} pt={0} mb={2}>
+          <Box cursor={"pointer"} onClick={handleLike}>
+            {!liked ? <NotificationsLogo/> : <UnlikeLogo/>}
+          </Box>
+          <Box cursor={"pointer"} fontSize={18}>
+            <CommentLogo/>
+          </Box>
+        </Flex>
+      )}
       <Text fontWeight={600} fontSize={"sm"} w={"full"}>
         {likes} likes
       </Text>
       {!isProfilePost && (
         <>
           <Text fontWeight={700} fontSize={"sm"} w={"full"}>
-            {username}{" "}
+            {/* {username}{" "} */}
           </Text>
           <Text fontWeight={400} fontSize={"sm"} w={"full"} cursor={"pointer"} color={"gray"}>
             View other comments
           </Text>
         </>
       )}
-      <Flex justifyContent={"space-between"} alignContent={"center"} gap={2} w={"full"}>
-        <InputGroup>
-          <Input variant={"flushed"} placeholder={"comment"} fontSize={14}/>
-          <InputRightElement>
-            <Button fontSize={14} color={"whiteAlpha.800"} cursor={"pointer"} fontWeight={600} bg={"transparent"}>Post</Button>
-          </InputRightElement>
-        </InputGroup>
-      </Flex>
+      {authUser &&( 
+        <Flex justifyContent={"space-between"} alignContent={"center"} gap={2} w={"full"}>
+                 
+            <InputGroup>
+              <Input variant={"flushed"} placeholder={"comment"} fontSize={14} 
+                  onChange={(e) => setComment(e.target.value)}
+			  	  			value={comment}
+			  	  			ref={commentRef}
+                />
+              <InputRightElement>
+                <Button fontSize={14} color={"whiteAlpha.800"} cursor={"pointer"} fontWeight={600} bg={"transparent"} isLoading={isUploading} onClick={handleSubmitComment}>Post</Button>
+              </InputRightElement>
+            </InputGroup>
+        </Flex>
+      )}
     </Box>
   )
 }
